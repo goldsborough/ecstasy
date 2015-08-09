eb11c82d3d054a210e059a92f5ccf6c544d50703
petergoldsborough <petergoldsborough@hotmail.com>
2015-08-09 02:09:17 +0200

    0.1.1

--- ecstasy/__init__.py
+++ ecstasy/__init__.py
@@ -4,7 +4,8 @@ from .parser import beautify			# noqa
 from .flags import Color, Fill, Style	# noqa
 
 __title__ = 'ecstasy'
-__version__ = '0.1'
+__version__ = '0.1.1'
+__url__ = "https://github.com/goldsborough/ecstasy"
 __author__ = 'Peter Goldsborough'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 Peter Goldsborough'
